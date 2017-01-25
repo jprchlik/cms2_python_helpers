85f15fa712fa1371180e04d603fdd1d406fa30c9
jprchlik <japrchlik@gmail.com>
2017-01-25 11:04:19 -0500

    Added default to read cms2_dir file for cms2 dir location for download wrapper

--- grab_sigmoid_download_wrapper.py
+++ grab_sigmoid_download_wrapper.py
@@ -18,7 +18,7 @@ def main(argv):
 
 #Descriptive information about each keyword
     argsdes=["A string time in the format of YYYY/MM/DD HH:MM:SS",
-             "The directory containing the CMS2",
+             "The directory containing the CMS2 (default= read 'cms2_dir' file",
              "The directory format for the sigmoid (assumes a subdirectory of cmsdir (default = YYYY/MM/DD/HHMM/"]
 
     for i in range(len(inargs2)): helpinfo = helpinfo+' -'+snargs1[i]+' <--'+inargs2[i]+'> : '+argsdes[i]+'\n'
@@ -32,6 +32,8 @@ def main(argv):
         sys.exit(2)
 #default for directory structure
     sigd = '%Y/%m/%d/%H%M/'
+#default for cms2 directory
+    cmsd = ''
 
 
     for opt, arg in opts:
